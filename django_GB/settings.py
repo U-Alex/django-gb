7b1a9d529497f726e0ac415dd28aeec81196140a
Alexander <uaa04@yandex.ru>
2024-02-28 03:20:28 +0900

    fix

--- django_GB/settings.py
+++ django_GB/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
-from .logging import LOGGING
+# from .logging import LOGGING
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -152,4 +152,45 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'formatter': 'verbose',
+            'filename': './log/django.log',
+            'encoding': 'utf-8',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'rnd_app': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'homework_1': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+        'verbose': {
+            # 'format': '{levelname} {asctime} {module} {process} {thread} {message}',
+            'format': '{levelname} {asctime} {name} {message}',
+            'style': '{',
+        },
+    },
+}
