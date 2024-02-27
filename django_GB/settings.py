46835f1dcdbe37dbc6c4de2198d9549964e53aa1
Alexander <uaa04@yandex.ru>
2024-02-28 03:36:26 +0900

    fix

--- django_GB/settings.py
+++ django_GB/settings.py
@@ -142,9 +142,9 @@ USE_TZ = False
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 # MEDIA_ROOT = str(BASE_DIR) + str(MEDIA_URL)
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
