29be1945cd42b103a2cf98feedfe2b9b2030e1b0
Alexander <uaa04@yandex.ru>
2024-02-28 03:50:11 +0900

    fix

--- django_GB/settings.py
+++ django_GB/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEBUG')
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
+# DEBUG = False
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
 
