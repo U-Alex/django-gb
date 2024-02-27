f0128f51829d0bfa0e63176a330af640f10018f5
Alexander <uaa04@yandex.ru>
2024-02-28 04:11:16 +0900

    fix

--- homework_2/management/commands/fake.py
+++ homework_2/management/commands/fake.py
@@ -8,26 +8,26 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
 
-        OrderStatus.objects.create(status_name='new')
-        OrderStatus.objects.create(status_name='paid')
-        OrderStatus.objects.create(status_name='delivery')
-        OrderStatus.objects.create(status_name='cancelled')
-
-        for i in range(10):
-            Client.objects.create(name=f"user_{i+1}",
-                                  email=f"user_{i+1}@mail.com",
-                                  phone=f"+7800{randint(1000000, 9999999)}",
-                                  address="Russia",
-                                  )
-        self.stdout.write(f'create {Client.objects.count()} clients')
-
-        for i in range(50):
-            Product.objects.create(name=f"product_{i+1}",
-                                   description=f"product_{i+1} description",
-                                   price=randint(1, 9999),
-                                   quantity=randint(1, 99),
-                                   )
-        self.stdout.write(f'create {Product.objects.count()} products')
+        # OrderStatus.objects.create(status_name='new')
+        # OrderStatus.objects.create(status_name='paid')
+        # OrderStatus.objects.create(status_name='delivery')
+        # OrderStatus.objects.create(status_name='cancelled')
+        #
+        # for i in range(10):
+        #     Client.objects.create(name=f"user_{i+1}",
+        #                           email=f"user_{i+1}@mail.com",
+        #                           phone=f"+7800{randint(1000000, 9999999)}",
+        #                           address="Russia",
+        #                           )
+        # self.stdout.write(f'create {Client.objects.count()} clients')
+        #
+        # for i in range(50):
+        #     Product.objects.create(name=f"product_{i+1}",
+        #                            description=f"product_{i+1} description",
+        #                            price=randint(1, 9999),
+        #                            quantity=randint(1, 99),
+        #                            )
+        # self.stdout.write(f'create {Product.objects.count()} products')
 
         ord_status = OrderStatus.objects.all()
         clients = Client.objects.all()
